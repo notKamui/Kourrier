e60e3dfabf050115f9e0296f796345c50850508d
notKamui <teillard.jimmy@outlook.fr>
2021-09-14 13:53:31 +0200

    Folder structure skeleton done.

---
+++ src/main/kotlin/com/notkamui/kourrier/core/imap/folder.kt
@@ -0,0 +1,107 @@
+package com.notkamui.kourrier.core.imap
+
+import com.sun.mail.imap.IMAPFolder
+import com.sun.mail.imap.IMAPMessage
+import javax.mail.FetchProfile
+import javax.mail.Folder
+import javax.mail.Message
+
+/**
+ * Wrapper around the standard [IMAPFolder].
+ */
+class KourrierFolder(private val imapFolder: IMAPFolder) {
+    private var profile = FetchProfile()
+
+    /**
+     * Closes the current [IMAPFolder], and [clean]s it or not (defaults to false).
+     */
+    fun close(clean: Boolean = false) {
+        imapFolder.close(clean)
+    }
+
+    /**
+     * Obtains the message at the given [index],
+     * or null if it doesn't exist.
+     */
+    operator fun get(index: Int): KourrierMessage? = imapFolder[index]
+
+    /**
+     * Obtains the list of message from the given [range] of index,
+     * or null if one of them doesn't exist.
+     * May [prefetch] the results (defaults to true).
+     */
+    operator fun get(range: IntRange, prefetch: Boolean = true): List<KourrierMessage>? {
+        val messages: Array<out Message> = try {
+            imapFolder.getMessages(range.toList().toIntArray())
+        } catch (e: IndexOutOfBoundsException) {
+            null
+        } ?: return null
+        if (prefetch) imapFolder.fetch(messages, profile)
+        return messages.map { KourrierMessage(it as IMAPMessage) }
+    }
+}
+
+/**
+ * Represents the different access modes to a [KourrierFolder].
+ */
+enum class FolderMode(private val mode: Int) {
+    /**
+     * Restrictive read permission.
+     */
+    ReadOnly(Folder.READ_ONLY),
+
+    /**
+     * Permissive write permission (+read).
+     */
+    ReadWrite(Folder.READ_WRITE),
+    ;
+
+    internal fun toMode(): Int = mode
+}
+
+/**
+ * Represents the different types of [KourrierFolder]s.
+ */
+enum class FolderType(private val type: Int) {
+    /**
+     * Holds only folders.
+     */
+    HoldsFolders(Folder.HOLDS_FOLDERS),
+
+    /**
+     * Holds only messages.
+     */
+    HoldsMessages(Folder.HOLDS_MESSAGES),
+
+    /**
+     * Holds both folders and messages.
+     */
+    HoldsAll(Folder.HOLDS_FOLDERS and Folder.HOLDS_MESSAGES),
+    ;
+
+    companion object {
+        /**
+         * Obtains a [FolderType] from the given [type].
+         */
+        fun fromType(type: Int): FolderType {
+            val holdsFolders = type and Folder.HOLDS_FOLDERS
+            val holdsMessages = type and Folder.HOLDS_MESSAGES
+            return when {
+                holdsFolders != 0 && holdsMessages != 0 -> HoldsAll
+                holdsFolders != 0 -> HoldsFolders
+                holdsMessages != 0 -> HoldsMessages
+                else -> throw UnknownFolderTypeException()
+            }
+        }
+    }
+}
+
+/**
+ * Is thrown when a [FolderType] is unknown or invalid.
+ */
+class UnknownFolderTypeException : Exception()
+
+private operator fun IMAPFolder.get(index: Int): KourrierMessage? =
+    (getMessage(index) as IMAPMessage?)?.let {
+        KourrierMessage(it)
+    }
---
+++ src/main/kotlin/com/notkamui/kourrier/core/imap/message.kt
@@ -0,0 +1,30 @@
+package com.notkamui.kourrier.core.imap
+
+import com.sun.mail.imap.IMAPFolder
+import com.sun.mail.imap.IMAPMessage
+
+/**
+ * Wrapper around the standard [IMAPMessage].
+ */
+class KourrierMessage(private val message: IMAPMessage) {
+    val uid: Long by lazy {
+        message.messageUID
+    }
+
+    val from: String by lazy {
+        message.from[0].toString()
+    }
+
+    val headers: List<KourrierMessageHeader> by lazy {
+        message.allHeaders.toList().map { KourrierMessageHeader(it.name, it.value) }
+    }
+
+    val body: String by lazy {
+        message.content as String
+    }
+}
+
+data class KourrierMessageHeader(val name: String, val value: String)
+
+private val IMAPMessage.messageUID: Long
+    get() = (folder as IMAPFolder).getUID(this)
